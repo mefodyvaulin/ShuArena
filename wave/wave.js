6d9a2f009cdf1659451f12fc16bd534fa5b49d2e
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2026-04-12 15:53:59 +0500

    to canvas

--- wave/wave.js
+++ wave/wave.js
@@ -1,44 +1,44 @@
-﻿const dotCount = 500;
-const dots = [];
+﻿const dotCount = 5000;
 let lastPos = new Float64Array(dotCount);
 let currentPos = new Float64Array(dotCount);
-const dt = 0.005;
+const dt = 0.0005;
 const startG = new Float64Array(dotCount).fill(0);
-const dotSize = 100 / dotCount;
 const c = 0.1;
 const r = c * dt / (1 / dotCount);
 const deltaH = 50;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 function waveStep(){
     const nextPos = [];
+    const w = canvas.clientWidth;
+    const h = canvas.clientHeight;
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = 'blue';
+
     for (let i = 0; i < dotCount; i++){
-        dots[i].style.top = `${currentPos[i]}%`;
+        const x = (i / dotCount) * w;
+        const y = (currentPos[i] / 100) * h;
+        ctx.fillRect(x, y, 2, 2);
+
         if (i === 0 || i === dotCount - 1){
             nextPos[i] = deltaH;
             continue;
         }
-        nextPos[i] = 2 * currentPos[i] - lastPos[i] + (r * r) * (currentPos[i + 1] - 2 * currentPos[i] + currentPos[i - 1]);
+        nextPos[i] = 2 * currentPos[i] - lastPos[i] +
+            (r * r) * (currentPos[i + 1] - 2 * currentPos[i] + currentPos[i - 1]);
     }
-    lastPos = [...currentPos];
-    currentPos = [...nextPos];
+
+    lastPos.set(currentPos);
+    currentPos.set(nextPos);
     requestAnimationFrame(waveStep);
 }
 
 function init(){
-    const container = document.querySelector('.wave');
     for (let i = 0; i < dotCount; i++){
-        let dot = document.createElement("div");
-        dots[i] = dot;
-        dot.className = "wave-item";
-        dot.style.left = `${i * dotSize}%`;
-        dot.style.width = `${dotSize}%`;
-
         const progress = i / (dotCount - 1);
         const damping = Math.sin(progress * Math.PI);
-        const startPos = deltaH + Math.sin(i * 0.1) * 5 * damping;
-
-        dot.style.top = `${startPos}%`;
-        lastPos[i] = startPos;
-        container.appendChild(dot);
+        lastPos[i] = deltaH + Math.sin(i * 0.1) * 5 * damping;
     }
 
     for (let i = 0; i < dotCount; i++){
@@ -52,4 +52,14 @@ function init(){
     requestAnimationFrame(waveStep);
 }
 
+function resizeCanvas(){
+    const dpr = window.devicePixelRatio || 1;
+    const rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+
+    ctx.scale(dpr, dpr);
+}
+
 init();
+resizeCanvas();
