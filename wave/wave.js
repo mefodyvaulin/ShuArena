1e0c962184c6d0cf1e60a3f98bc1f8526a789a81
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2026-04-12 16:23:41 +0500

    refactor

--- wave/wave.js
+++ wave/wave.js
@@ -9,47 +9,60 @@ const deltaH = 50;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-function waveStep(){
-    const nextPos = [];
+function drawCurrent() {
     const w = canvas.clientWidth;
     const h = canvas.clientHeight;
     ctx.clearRect(0, 0, w, h);
     ctx.fillStyle = 'blue';
-
-    for (let i = 0; i < dotCount; i++){
+    for (let i = 0; i < dotCount; i++) {
         const x = (i / dotCount) * w;
         const y = (currentPos[i] / 100) * h;
         ctx.fillRect(x, y, 2, 2);
+    }
+}
 
-        if (i === 0 || i === dotCount - 1){
-            nextPos[i] = deltaH;
-            continue;
-        }
+function calculateNext() {
+    const nextPos = new Float64Array(dotCount);
+    for (let i = 0; i < dotCount; i++) {
+
+
         nextPos[i] = 2 * currentPos[i] - lastPos[i] +
             (r * r) * (currentPos[i + 1] - 2 * currentPos[i] + currentPos[i - 1]);
+        if (i === 0 || i === dotCount - 1) nextPos[i] = deltaH;
     }
-
     lastPos.set(currentPos);
     currentPos.set(nextPos);
+}
+
+function waveStep(){
+    drawCurrent();
+    calculateNext();
+
     requestAnimationFrame(waveStep);
 }
 
-function init(){
+function fillInitLayer(){
     for (let i = 0; i < dotCount; i++){
         const progress = i / (dotCount - 1);
         const damping = Math.sin(progress * Math.PI);
         lastPos[i] = deltaH + Math.sin(i * 0.1) * 5 * damping;
     }
+}
 
+function fillFirstLayer(){
     for (let i = 0; i < dotCount; i++){
         if (i === 0 || i === dotCount - 1){
             currentPos[i] = deltaH;
             continue;
         }
-        currentPos[i] = lastPos[i] + dt * startG[i] + (r * r) / 2 * (lastPos[i + 1] - 2 * lastPos[i] + lastPos[i - 1]);
+        currentPos[i] = lastPos[i] + dt * startG[i] +
+            (r * r) / 2 * (lastPos[i + 1] - 2 * lastPos[i] + lastPos[i - 1]);
     }
+}
 
-    requestAnimationFrame(waveStep);
+function init(){
+    fillInitLayer();
+    fillFirstLayer()
 }
 
 function resizeCanvas(){
@@ -63,3 +76,4 @@ function resizeCanvas(){
 
 init();
 resizeCanvas();
+requestAnimationFrame(waveStep);
